28465a4d0f8710deb493baeb49691b9e9993dba7
zenn24ct <158288013+zenn24ct@users.noreply.github.com>
2025-09-23 06:35:45 +0000

    githubに繋ぐためにコード修正

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/update-my-portfolio/",  // ←リポジトリ名に合わせる
 })
+
+
+
